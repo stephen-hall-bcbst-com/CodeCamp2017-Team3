e3074810e2d63806c450e18a4d8c484b057662ff
em-cuthy <emelia.cuthbertson@gmail.com>
2017-07-19 13:46:41 -0400

    pause added

--- CodeCamp-LoggedIn.js
+++ CodeCamp-LoggedIn.js
@@ -10,16 +10,20 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-        // split message into variables
-        var message1 = "Hello, I'm T.O.M., Technical Operating Memory. ";
-        var message2 = "Thank you for taking time out of your day to talk to me. ";
-        var message3 = "You can ask me things like my life story or who will win this competition. ";
 
-    // reconstruct message
-    message1.concat(message2 + message3);
 
         // followup message
-        Slack.postMessageToChannel(channel, "I can even tell you some jokes!");
+
+        var newState;
+        Slack.postMessageToChannel(channel, "Hello, I'm T.O.M., Technical Operating Memory. ");
+        Slack.postMessageToChannel(channel, "Ask me questions like, who will win this competition or what is my life story. ");
+         setTimeout(function() {
+        if (newState == -1) {
+            Slack.postMessageToChannel(channel, "I can even tell you some jokes!");
+        }
+    }, 500);
+
+        
 
         // *********************************************************************
         // STOP CODING!
