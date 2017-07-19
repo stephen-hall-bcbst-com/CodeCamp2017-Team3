daaa17a82f1e581bc76969dcab038de3cabd2d78
em-cuthy <emelia.cuthbertson@gmail.com>
2017-07-19 09:34:44 -0400

    vars

--- CodeCamp-LoggedIn.js
+++ CodeCamp-LoggedIn.js
@@ -10,7 +10,12 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-        Slack.postMessageToChannel(channel, "Hello, I'm T.O.M., Technical Operating Memory. It's been so long since someone has talked to me. You can ask me questions, like what is my life story, or who will win this competition.");
+        var message1 = "Hello, I'm T.O.M., Technical Operating Memory. ";
+        var message2 = "Thank you for taking time out of your day to talk to me. ";
+        var message3 = "You can ask me things like my life story or who will win this competition.";
+
+    message1.concat(message2);
+    message2.concat(message3);
 
         // *********************************************************************
         // STOP CODING!
