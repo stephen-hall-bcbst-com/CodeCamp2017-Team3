4283caec658f46c1f59f280640c93bf33503dccf
em-cuthy <emelia.cuthbertson@gmail.com>
2017-07-19 10:28:24 -0400

    concatenation

--- CodeCamp-LoggedIn.js
+++ CodeCamp-LoggedIn.js
@@ -10,12 +10,16 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
+        // split message into variables
         var message1 = "Hello, I'm T.O.M., Technical Operating Memory. ";
         var message2 = "Thank you for taking time out of your day to talk to me. ";
-        var message3 = "You can ask me things like my life story or who will win this competition.";
+        var message3 = "You can ask me things like my life story or who will win this competition. ";
 
-    message1.concat(message2);
-    message2.concat(message3);
+    // reconstruct message
+    message1.concat(message2 + message3);
+
+        // followup message
+        Slack.postMessageToChannel(channel, "I can even tell you some jokes!");
 
         // *********************************************************************
         // STOP CODING!
