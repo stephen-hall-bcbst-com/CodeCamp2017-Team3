55c809fd2a2d7b643493b2c3106a29ed3724c63d
em-cuthy <emelia.cuthbertson@gmail.com>
2017-07-19 09:05:12 -0400

    switch messages

--- CodeCamp-LoggedIn.js
+++ CodeCamp-LoggedIn.js
@@ -10,9 +10,8 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-        Slack.postMessageToChannel(channel, "Hello, I'm T.O.M., Technical Operating Memory. It's been so long since someone has talked to me.");
-
         Slack.postMessageToChannel(channel, "You can ask me questions, like what is my life story, or who will win this competition.");
+        Slack.postMessageToChannel(channel, "Hello, I'm T.O.M., Technical Operating Memory. It's been so long since someone has talked to me.");
 
         // *********************************************************************
         // STOP CODING!
