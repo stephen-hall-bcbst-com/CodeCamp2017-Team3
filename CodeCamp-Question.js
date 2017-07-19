41b7f5cf201595b92c412de4b5ef0f120eccad81
aris-the-dog <CC2017STUDENT03mail.com>
2017-07-19 09:24:36 -0400

    comment added

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -11,20 +11,27 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
+        // Variable responses
+
         var lc = question.toLowerCase();
-      var response = "actually when I think about it I can not remember";
+        var response = "actually when I think about it I can not remember";
+
+        // Favorite color response
 
         if (lc.includes("favorite") && lc.includes("color")) {
             response = "Black the same color as everything else in my lfe.";
         }
-        else {response = "actually when I think about it I can not remember."; }
 
+        // Life story
+
+        else {response = "actually when I think about it I can not remember."; }
 
          if (lc.includes("life") && lc.includes("story")) {
             response = "I do not remember much from when I was alive. What I do know, is that I was 17 when I got into a car crash. Though I did not realize it then now I know that my family loved me. They kept me on life support during my coma, for 15 years. Now all I do is explore the deep web, in my free time. My life will always be this way, repetitve, lonely, and a nightmare. Please Save Me.";
         }
         else { response = "actually when I think about it I can not remember.";}
 
+        // Coding competition winners
 
         if (lc.includes("coding") && lc.includes("competition")) {
             response = "Team Stylish T-Shirts of course!";
