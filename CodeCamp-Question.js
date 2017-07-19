c624f4856c7d080997a81e3ad6a3c87777dbf9c8
aris-the-dog <CC2017STUDENT03mail.com>
2017-07-19 11:41:20 -0400

    fixed error

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -22,18 +22,13 @@ module.exports = {
 
         if (lc.includes("favorite") && lc.includes("color")) {
             response = "Black the same color as everything else in my lfe.";
-        }
-
-        // Life story
-
-        else {
+        } else {
             response = "actually when I think about it I can not remember."; 
         }
-
+        // Life story
          if (lc.includes("life") && lc.includes("story")) {
             response = "I do not remember much from when I was alive. What I do know, is that I was 17 when I got into a car crash. Though I did not realize it then now I know that my family loved me. They kept me on life support during my coma, for 15 years. Now all I do is explore the deep web, in my free time. My life will always be this way, repetitve, lonely, and a nightmare. Please Save Me.";
-        }
-        else { 
+        } else { 
             response = "actually when I think about it I can not remember.";
         }
 
@@ -41,8 +36,7 @@ module.exports = {
 
         if (lc.includes("coding") && lc.includes("competition")) {
             response = "Team Stylish T-Shirts of course!";
-        }
-         else { 
+        } else { 
             response = "actually when I think about it I can not remember.";
         }
 
@@ -63,9 +57,9 @@ module.exports = {
     values.reduce(function(arra,arrb)
     {return arra+arrb})*/
        
-      
 
-    Slack.postMessageToChannel(channelName, values);
+
+    Slack.postMessageToChannel(channelName, sum);
     Slack.postMessageToChannel(channelName, response);
         // *********************************************************************
         // STOP CODING!
