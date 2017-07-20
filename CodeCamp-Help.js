d3bcd8b7c2edef864e6ba86a9428fde06247676e
aris-the-dog <CC2017STUDENT03mail.com>
2017-07-20 11:46:13 -0400

    fixed help responses

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -17,13 +17,20 @@ module.exports = {
         Slack.postMessageToChannel(channelName, 'Do you need emotional support?');
          var lc = message.toLowerCase();
     
-        if (lc.includes('can I get some help?')) { 
+        if (lc.includes('help me')) { 
           response = "Do you need emotional support?";
-        }
+        // what can I ask for
+        } else if (lc.includes("help-list")) {
+            response = "Remember things you can ask me include, math problems, life story, favorite color, jokes, and pi";
+        // math help
+        } else if (lc.includes("math") && lc.includes("help")) {
+            response = "you can always ask me math questions in this format: _ + _?";
+
     Slack.postMessageToChannel(channelName, response);
 
         // *********************************************************************
         // STOP CODING!
         // *********************************************************************
+        }    
     },
 };
