7db07ec3f85a999ecf0d56185c819d6a43c2fd42
em-cuthy <emelia.cuthbertson@gmail.com>
2017-07-20 09:04:03 -0400

    comments added

--- CodeCamp-LoggedIn.js
+++ CodeCamp-LoggedIn.js
@@ -10,12 +10,15 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
     
+        // introduction
         Slack.postMessageToChannel(channel, "Hello, I'm T.O.M., Technical Operating Memory. ");
 
+        // pause between messages
         setTimeout(function() {
                 Slack.postMessageToChannel(channel, "Ask me questions like, who will win this competition or what is my life story. ");
         }, 2000);
-            
+
+        // pause between messages    
         setTimeout(function() {
                 Slack.postMessageToChannel(channel, "I can even tell you some jokes!");
         }, 4000);
