0863700eb01320787352e9f4de56280b6734d5eb
cova Melendez <covarey919@gmail.com>
2017-07-20 13:27:40 -0400

    Message Update

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -12,30 +12,28 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
        var response;
-        Slack.postMessageToChannel(channelName, "You said something?");
+        // Slack.postMessageToChannel(channelName, "You said something?");
          var lc = message.tolowercase();
          var Uc = message.toUpperCase();
-
-        if (lc.includes('music')); {
+        response = " G";
+        
+        if (lc.includes("music") && lc.includes("favorite")) {
             response = "look up the first daredevil movie's soundtrack";
-        }
-
-        if (lc.includes('clothing')) {
-            response = "I like dark colors, makes it harder for people to see me.";
-        }
-
-        if (Uc.includes('HEY!!!')) {
+        } if (lc.includes("clothing") && lc.includes('type')) {
+            response = "I like dark colors,makes it harder for people to see me.";
+        } if (lc.includes('+')) {
+            response = "Get a calculator";
+        } if (Uc.includes("HEY!!!")) {
             response = "Can you relax diaper baby!!!";
-        }
-
-        if (lc.includes('hello')) {
+        } if (lc.includes("screw you bro")) {
+            response = "you can die,I can't,get gud";
+        } if (lc.includes("hello")) {
             response = "hey...";
         }
-        Slack.postMessageToChannel(channelName, response);
-
-        
+         Slack.postMessageToChannel(channelName, response);
+         Slack.postMessageToChannel(channelName, "You said something?");
         // *********************************************************************
         // STOP CODING!
         // *********************************************************************
-    },
+ },
 };
