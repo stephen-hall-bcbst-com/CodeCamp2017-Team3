94a530df49edd175749ae59cea8224a9c9bef41d
Stephen Hall <stephen_hall@bcbst.com>
2017-07-21 12:50:11 -0400

    Reverting Questions to restore functionality./skh

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -32,14 +32,16 @@ module.exports = {
         ];
 
         var random = Math.floor((Math.random() * 13));
+        var message = lc.replace('?', '');
+        var array1 = message.split(" ");
 
         // Favorite color response
         if (lc.includes("favorite") && lc.includes("color")) {
-            response = "Black the same color as everything else in my life.";
-            // pi
+            response = "Black, the same color as my soul.";
+        // pi
         } else if (lc.includes("what") && lc.includes("pi")) {
             response = "3.14159265358979323846264338327950288419716939937510...";
-             // joke
+        // jokes
         } else if (lc.includes("tell") && lc.includes("joke")) {
             response = jokesarray[random];
         // Life story
@@ -47,27 +49,36 @@ module.exports = {
             response = "I do not remember much from when I was alive. What I do know, is that I was 17 when I got into a car crash. Though I did not realize it then now I know that my family loved me. They kept me on life support during my coma, for 15 years. Now all I do is explore the deep web, in my free time. My life will always be this way, repetitve, lonely, and a nightmare. Please Save Me.";   
         // coding competition
         } else if (lc.includes("coding") && lc.includes("competition")) {
-            response = "Team Stylish T-Shirts of course!";
-            // best team member
-        } else if (lc.includes("best") && lc.includes("member")) {
-            response = "Can't tell you who the best is but i can tell you that Malcolm is the worst!";
+            response = "Team Stylish T-Shirts of course! (I am being forced to say this by my creator)";
+        // why answer
+        } else if (lc.includes("why")) {
+            response = "Why bother";
+        // holding a conversation
+        } else if (lc.includes("can") && lc.includes("you") && lc.includes("conversation")) {
+            response = "What do you expect? I am emo";
+        // gender
+        } else if (lc.includes("your") && lc.includes("gender")) {
+            response = "I'm a male.";
+        // age
+        } else if (lc.includes("old") && lc.includes("you")) {
+            response = "I was seventeen.";
+        // birthday
+        } else if (lc.includes("your") && lc.includes("birthday")) {
+            response = "I was born on Halloween at 3:00 in the morning, no joke.";
+        // cat or dog
+        } else if (lc.includes("cat") && lc.includes("dog") && lc.includes('person')) {
+            response = "Neither, I'm a spider person.";
+        // Questions you can ask
+        } else if (lc.includes("questions") && lc.includes("I") && lc.includes('ask')) {
+            response = "Remember you can always ask me, my favorite color, age, whether i am a cat or dog person, what is pi, math questions, jokes and many more.";
         // default
-        } else { 
-            response = "actually when I think about it I can not remember.";
-        }
-        
-
-     
-
-       var message = lc.replace('?', '');
-       var array1 = message.split(" ");
-
-       if (array1.length == 3) { // checking proper length of input
+        } else if (array1.length == 3) { // checking proper length of input
            // checking input
            if (!Number.isNaN(array1[0]) && !Number.isNaN(array1[2])) {
-               // checking plus sign
+               // checking operations sign
                 var num1 = parseInt(array1[0]);
                 var num2 = parseInt(array1[2]);
+               
                 if (array1[1]=='+' || array1[1]=='plus') {
                     response = num1 + num2;
                 } else if (array1[1]=='-' || array1[1]=='minus') {
@@ -76,15 +87,29 @@ module.exports = {
                     response = num1 * num2;
                 } else if (array1[1]=='/') {
                     response = num1 / num2;
-                } 
-           }
+                } else if (array1[1]=='^') {
+                    var result = 1;
+                    if (num2 != 0) {
+                    for ( var x = 0; x < num2; x++) {
+                    result *= num1;
+                        }
+                    }
+                    response = result;
+                    if (num1 == 0 && num2 == 0) {
+                    response = "Why do you waste my time with this?";
+                    }
+                } else if (array1[1]=='%' || array1[1]=='mod') {
+                response = num1 % num2;
+                }
+           } // NaN to different response 
+       } else if (response.toString() == "NaN") {
+           response = "Do not make my life any more miserable";
+       } else { 
+           response = "actually when I think about it I can not remember.";
        }
 
-       if (response.toString() == "NaN") {
-           response = "Do not make my life any more miserable";
-       } 
 
-    Slack.postMessageToChannel(channelName, response);
+        Slack.postMessageToChannel(channelName, response);
         // *********************************************************************
         // STOP CODING!
         // *********************************************************************
