a56dc8db41250634f0cc3754b197d6b9b02595aa
Carter Davis <cartercdavis@gmail.com>
2017-07-21 09:32:44 -0400

    commit

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -16,19 +16,20 @@ module.exports = {
          var response;    
         
          var lc = message.toLowerCase();
-    
+        
         if (lc.includes('me')) { 
           response = "Do you need emotional support?";
-        // what can I ask for
+        // You can ask for jokes,help with math problems,and favorite colors.
         } else if (lc.includes("list")) {
-            response = "Remember things you can ask me include, math problems, life story, favorite color, jokes, and pi";
-        // math help
+            response = "Remember things you can ask me include, math problems, ";
+            "life story, favorite color, jokes, and pi";
+        // What format of math T.O.M. can use and how he can help.
         } else if (lc.includes("math")) {
             response = "you can always ask me math questions in this format: a + b?";
         } else {
             response = 'With what?';
         }
-   
+        // 
     Slack.postMessageToChannel(channelName, response);
 
         // *********************************************************************
