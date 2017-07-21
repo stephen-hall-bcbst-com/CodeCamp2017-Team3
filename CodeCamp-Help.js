bd35e82946cb8c48671e0e7a61333f244ae074f9
Carter Davis <cartercdavis@gmail.com>
2017-07-21 11:23:41 -0400

    updating comments

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -16,15 +16,16 @@ module.exports = {
          var response;    
         
          var lc = message.toLowerCase();
-        // emotional help response
+        
         if (lc.includes('me')) { 
           response = "Do you need emotional support?";
         // You can ask for jokes,help with math problems,and favorite colors.
         } else if (lc.includes("list")) {
-            response = "Remember things you can ask me include, math problems, life story, favorite color, jokes, pi, etc.";
-        // math help
+            response = "Remember things you can ask me include, math problems, " +
+            "life story, favorite color, jokes, and pi";
+        // What format of math T.O.M. can use and how he can help.
         } else if (lc.includes("math")) {
-            response = "you can always ask me math questions in this format: a + b? (any operation even mod)";
+            response = "you can always ask me math questions in this format: a + b?";
         } else {
             response = 'with what?';
         }
